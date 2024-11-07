f6f798962b70a4ae51f8431f7fc0145f507294c7
maula1243 <elhaqhilmimaula@gmail.com>
2024-11-07 13:34:53 +0700

    Add files via upload

---
+++ condition_Maula/script.js
@@ -0,0 +1,13 @@
+document.getElementById("orderbutton").onclick = function () {
+    let foodPrice = parseInt(document.getElementById("food").value);
+    let drinkPrice = parseInt(document.getElementById("drink").value);
+
+    let totalprice = foodPrice + drinkPrice;
+
+    let discount = 0;
+
+    if (totalprice > 30) {
+        alert(("ok"))
+    }
+
+}
